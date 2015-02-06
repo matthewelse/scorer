4b56af1f83eed2e93c6dc96f4d7e4c8e93dc0d79
Matthew Else <matthewelse1997@gmail.com>
2015-02-06 12:33:01 +0000

    Fix a few bugs

--- scorer/models.py
+++ scorer/models.py
@@ -1,19 +1,6 @@
-# Copyright (c) 2015, Matthew Else matthewelse1997@gmail.com
-
-# Permission to use, copy, modify, and/or distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
-
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
 from django.db import models
 
+# Create your models here.
 
 class Event(models.Model):
     name = models.CharField(max_length=30)
@@ -38,8 +25,14 @@ class Team(models.Model):
 
         return sum(scores)
 
+    def position_raw(self):
+        current_event = Event.objects.get(active=True)
+        scores = list(sorted([team.score() for team in Team.objects.filter(event=current_event)], reverse=True))
+        return scores.index(self.score()) + 1
+
     def position(self):
-        scores = [team.score() for team in Team.objects.all()]
+        current_event = Event.objects.get(active=True)
+        scores = list(sorted([team.score() for team in Team.objects.filter(event=current_event)], reverse=True))
         return scores.index(self.score()) + 1, scores.count(self.score()) >= 2
 
     def position_pretty(self):
